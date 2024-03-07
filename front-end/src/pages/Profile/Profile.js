38c27565c0b3b5bbc67ae47dc3c1c68ef417e971
nrezhang <henryszhang83@gmail.com>
2024-03-07 00:48:35 -0500

    images and axios

--- front-end/src/pages/Profile/Profile.js
+++ front-end/src/pages/Profile/Profile.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './Profile.css'
 
 const Profile = () => {
+
+  const pfp= ['https://picsum.photos/id/237/200/300']
+  const yourName = 'Tony_Gunk'
+  const bio = 'This is a bio lorem ipsum blah blah I like to walk my dog and feed my dog. I also love cats but I lvoe dogs more. I live in NYC. '
+  const [images, setImages] = useState([])
+  const [error, setError] = useState('')
+
+
+
+  const fetchImages = () => {
+    axios
+      .get(`https://picsum.photos/v2/list`)
+      .then(response => {
+        const images = response.data
+        setImages(images)
+        console.log(images)
+      })
+      .catch(err => {
+        const errMsg = JSON.stringify(err, null, 2) // convert error object to a string so we can simply dump it to the screen
+        setError(errMsg)
+      })
+  }
+
+  useEffect(() => {
+    fetchImages()
+    const intervalHandle = setInterval(() => {
+      fetchImages()
+    }, 5000)
+    return e => {
+      clearInterval(intervalHandle)
+    }
+  }, []) 
+
+
   return (
-    <div>Profile</div>
+    <>
+      <div className='header'>
+        <div className = 'profileHeader'>
+            <img src={pfp} alt='avatar' />
+            <div className='userName'>
+              <h1>{yourName}</h1>
+            </div>
+        </div>
+        <div className = 'bio'>
+          <p>{bio}</p>
+        </div>
+
+      <button> Edit Profile </button>
+      <div className='profilePosts'>
+      {
+        images.map(image => (
+            <div className='imageContainer'>
+              <img key= {image.id} src={image.download_url} alt='image' />
+            </div>
+        ))
+      }
+      </div>
+      </div>
+      <div>
+
+      </div>
+    </>
   )
 }
 

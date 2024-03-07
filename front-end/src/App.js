9b73724e48328bbb42413a91cb792b508afd14b4
Kyoung655 <kay271@nyu.edu>
2024-03-07 12:45:48 -0500

    fixed front-end conflicts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import Navbar from './pages/NavBar/NavBar';
 import Message from './pages/Message/Message';
 import Swipe from './pages/Swipe/Swipe';
 import Add from './pages/Add/Add';
 import Profile from './pages/Profile/Profile';
 import Discover from './pages/Discover/Discover';
-import Login from './pages/Login/Login';
+import Feed from "./pages/Feed/Feed"
+import EditProfile from './pages/Profile/EditProfile';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/feed" />} />
           <Route path="/swipe" element={<Swipe/>} />
+          <Route path="/feed" element={<Feed />} />
           <Route path="/discover" element={<Discover/>} />
           <Route path="/add" element={<Add/>} />
           <Route path="message" element={<Message/>} />
@@ -21,10 +24,10 @@ function App() {
           <Route path="/login" element={<Login/>} />
           {/* add pages above */}
         </Routes>
-        <Navbar/>
+        <Navbar />
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

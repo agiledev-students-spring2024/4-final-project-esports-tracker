2146e8688783a5eb5a8acf6073e86f93310f8716
nrezhang <henryszhang83@gmail.com>
2024-03-07 10:54:32 -0500

    added initial card layout

---
+++ front-end/src/pages/Swipe/Card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Card.css'
+import { IoHeartOutline, IoChatbubbleEllipsesOutline, IoEllipsisHorizontalOutline} from "react-icons/io5";
+
+const Card = () => {
+
+    const image = 'https://picsum.photos/200/300'
+
+  return (
+    <>
+    <div className="card" style = {{backgroundImage: `url(${image})`}}>
+            <div className = 'cardOverlay'>
+                <div className="cardInfo">
+                    <h2>Pet Name</h2>    
+                    <h4>Breed</h4>        
+                    <h4>Age/Weight</h4>
+                    <h4>Location</h4>
+                    <h4>Description</h4>
+                </div>
+            `   <div className="cardButtons">
+                        <IoHeartOutline/>
+                        <IoChatbubbleEllipsesOutline/>
+                        <IoEllipsisHorizontalOutline/>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Card

a84977f5a96afaa9bdb5b35803939d8235c09f46
Natalie Wu <nw2132@nyu.edu>
2024-03-07 12:19:44 -0500

    log in page + tailwind config

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Login.css'
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    console.log('Login Attempt with:', { email, password });
+ 
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="login-button">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;

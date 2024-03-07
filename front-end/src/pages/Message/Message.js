b996b8f3bb66640e85c53ad2cc35f1363cbe4244
nrezhang <henryszhang83@gmail.com>
2024-03-06 23:29:48 -0500

    basic post page

--- front-end/src/pages/Message/Message.js
+++ front-end/src/pages/Message/Message.js
@@ -26,7 +26,7 @@ const Message = () => {
     </div>
 
     {/* change this */}
-    <div className='Search'>
+    <div className='search'>
       <input type='text' placeholder="search" name="search" 
         value={search}
         onChange={handleSubmit}

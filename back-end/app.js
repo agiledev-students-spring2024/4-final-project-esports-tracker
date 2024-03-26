e8de9c0950aa1b00039ab8e3412c49fe86e7e133
nrezhang <henryszhang83@gmail.com>
2024-03-26 00:09:18 -0400

    class slides

---
+++ back-end/app.js
@@ -0,0 +1,7 @@
+// import and instantiate express
+const express = require("express") // CommonJS import style!
+const app = express() // instantiate an Express object
+
+// we will put some server logic here later...
+
+module.exports = app

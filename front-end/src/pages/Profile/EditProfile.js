176ed05df74942aa353a25f31820f24cd7045ac1
nrezhang <henryszhang83@gmail.com>
2024-03-28 10:46:52 -0400

    routes for edit profile

--- front-end/src/pages/Profile/EditProfile.js
+++ front-end/src/pages/Profile/EditProfile.js
@@ -1,21 +1,39 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './EditProfile.css'
 import { IoChevronBack } from "react-icons/io5";
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 
 const EditProfile = () => {
 
-
+    const[pfp, setPfp] = useState('https://picsum.photos/id/237/200/300'); //default image
     const [username, setUsername] = useState('');
     const [bio, setBio] = useState('');
     const [email, setEmail] = useState('');
 
+    useEffect(() => {
+        async function fetchData() {
+        const req = await axios.get('http://localhost:3001/profile/profile')
+        .then((response) => {
+          setUsername(response.data.username);
+          setBio(response.data.bio);
+          setEmail(response.data.email);
+          setPfp(response.data.pfp);
+        })
+        .catch((error) => {
+          console.error('Error fetching posts:', error)
+        })}
+        fetchData()
+      },[])
     
     const handleUser = (event) => {
         const inputValue = event.target.value;
         setUsername(inputValue);
+      }
+      const handleEmail = (event) => {
+        const inputValue = event.target.value;
+        setEmail(inputValue);
       }
       const handleBio = (event) => {
         const inputValue = event.target.value;
@@ -26,11 +44,40 @@ const EditProfile = () => {
           alert('Bio is too long');
         }
       };
+      const handleImage = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setPfp(reader.result);
+          };
+          reader.readAsDataURL(file);
+        }
+      };
 
-    const handleSubmit = () => {
+
+      const handleSubmit = (event) => {
         //send the caption and image data to a server
-        //do later
-        window.location.href = '/Profile';
+        const ret = {
+          username: username,
+          bio: bio,
+          email: email,
+          pfp: pfp,
+          preferences: {// change this later
+            breed: 'dog'
+          }
+          
+        }
+        console.log(ret)
+        axios.post('http://localhost:3001/profile/editProfile', ret)
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.error('Error handling out of frame data:', error)
+        })
+        alert('Changes saved');
+        // window.location.href = '/Profile';
       };
 
 
@@ -46,21 +93,28 @@ const EditProfile = () => {
 
 <div className = "editInputs">
     <div className = 'inputImage'>
-        <img src='https://picsum.photos/id/237/200/300' alt='profile' />
+        {pfp && (
+            <div>
+              <img src={pfp} alt="Selected" style={{ maxWidth: '100%' }} />
+            </div>
+          )}
+        <input type='file' onChange={handleImage} accept='image/*' />
     </div>
+
     <div className = 'inputItem'>
         <h2>Username: </h2>
-        <input type='text' placeholder="Username" name="username" onChange={handleUser}/>
+        <input type='text' placeholder="Username" name="username" value={username} onChange={handleUser}/>
     </div>
     <div className = 'inputItem'>
         <h2>Bio: </h2>
-        <textarea type='text' placeholder="Bio" name="bio" onChange={handleBio}/>
+        <textarea type='text' placeholder="Bio" value={bio} name="bio" onChange={handleBio}/>
     </div>
     <div className = 'inputItem'>
         <h2>Email: </h2>
-        <input type='text' placeholder="Email" name="email" />
+        <input type='text' placeholder="Email" value={email} name="email" onChange={handleEmail} />
     </div>
-    <div className = 'inputItem'>
+    <div className = 'inputItem'> 
+    {/* TODO: Add a dropdown menu for preferences integrated with backend */}
         <h2>Preferences: </h2>
         <select name="preferences" id="preferences">
             <option value="1">Dog</option>

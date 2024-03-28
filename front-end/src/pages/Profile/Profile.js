fe6c3cd693be163a777827f01965a247adfc0710
Natalie Wu <nw2132@nyu.edu>
2024-03-28 11:13:24 -0400

    front end connext profile

--- front-end/src/pages/Profile/Profile.js
+++ front-end/src/pages/Profile/Profile.js
@@ -1,73 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import './Profile.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './Profile.css';
 
 const Profile = () => {
+  const [profile, setProfile] = useState({
+    username: '',
+    bio: '',
+    pfp: ''
+  });
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
 
-  const pfp= ['https://picsum.photos/id/237/200/300']
-  const yourName = 'Tony_Gunk'
-  const bio = 'This is a bio lorem ipsum blah blah I like to walk my dog and feed my dog. I also love cats but I lvoe dogs more. I live in NYC. '
-  const [images, setImages] = useState([])
-  const [error, setError] = useState('')
-
-
-
-  const fetchImages = () => {
-    axios
-      .get(`https://picsum.photos/v2/list`)
+  // Fetch profile data from the server
+  useEffect(() => {
+    axios.get('/profile')
       .then(response => {
-        const images = response.data
-        setImages(images)
-        console.log(images)
+        setProfile({
+          username: response.data.username,
+          bio: response.data.bio,
+          pfp: response.data.pfp
+        });
       })
       .catch(err => {
-        const errMsg = JSON.stringify(err, null, 2) // convert error object to a string so we can simply dump it to the screen
-        setError(errMsg)
-      })
-  }
+        setError('Failed to load profile.');
+      });
 
-  useEffect(() => {
-    fetchImages()
-    const intervalHandle = setInterval(() => {
-      fetchImages()
-    }, 5000)
-    return e => {
-      clearInterval(intervalHandle)
-    }
-  }, []) 
+    const fetchImages = () => {
+      axios.get(`https://picsum.photos/v2/list`)
+        .then(response => {
+          setImages(response.data);
+        })
+        .catch(err => {
+          setError('Failed to load images.');
+        });
+    };
 
+    fetchImages();
+    const intervalHandle = setInterval(fetchImages, 5000);
+
+    return () => clearInterval(intervalHandle);
+  }, []);
 
   return (
     <>
       <div className='header'>
-        <div className = 'profileHeader'>
-            <img src={pfp} alt='avatar' />
-            <div className='userName'>
-              <h1>{yourName}</h1>
-            </div>
+        <div className='profileHeader'>
+          <img src={profile.pfp} alt='avatar' />
+          <div className='userName'>
+            <h1>{profile.username}</h1>
+          </div>
         </div>
-        <div className = 'bio'>
-          <p>{bio}</p>
+        <div className='bio'>
+          <p>{profile.bio}</p>
         </div>
-      <Link to='/editProfile'>
-      <button className='editProfileButton'> Edit Profile </button>
-      </Link>
-      <div className='profilePosts'>
-      {
-        images.map(image => (
-            <div className='imageContainer'>
-              <img key= {image.id} src={image.download_url} alt='image' />
+        <Link to='/editProfile'>
+          <button className='editProfileButton'> Edit Profile </button>
+        </Link>
+        <div className='profilePosts'>
+          {images.map(image => (
+            <div className='imageContainer' key={image.id}>
+              <img src={image.download_url} alt='image' />
             </div>
-        ))
-      }
-      </div>
-      </div>
-      <div>
-
+          ))}
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;

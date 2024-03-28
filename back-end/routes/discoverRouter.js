40ae53d80d9ffc4e3adc1361bd9f1e28f2fed4df
afknero <en1135@nyu.edu>
2024-03-28 02:54:03 -0400

    Add discover route and connect to front-end

---
+++ back-end/routes/discoverRouter.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const router = express.Router()
+
+// hard-coded markers data
+const markers = [
+  {
+    id: 1,
+    geocode: [40.7309, -73.9965],
+    popUp: "childrens park",
+  },
+  {
+    id: 1,
+    geocode: [40.73075, -73.9985],
+    popUp: "dog park",
+  },
+  {
+    id: 1,
+    geocode: [40.7304, -73.9976],
+    popUp: "dog park",
+  },
+  {
+    id: 2,
+    geocode: [40.73075, -73.9985],
+    popUp: "dog park",
+  },
+  {
+    id: 2,
+    geocode: [40.7304, -73.9976],
+    popUp: "dog park",
+  },
+]
+
+router.get("/discover/:id", (req, res) => {
+  const { id } = req.params
+  // filter markers based on the provided id
+  const filteredMarkers = markers.filter((marker) => marker.id == parseInt(id))
+  // return 200 with an empty array if no markers are found
+  if (filteredMarkers.length === 0) {
+    return res.status(200).json([])
+  }
+  res.json(filteredMarkers)
+})
+
+module.exports = router

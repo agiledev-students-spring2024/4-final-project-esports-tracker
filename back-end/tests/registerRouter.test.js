8761014f42af3227d2c60c27feec47e0770a9971
Kyoung655 <kay271@nyu.edu>
2024-04-02 09:13:23 -0400

    added some tests for registerRouter

--- back-end/tests/registerRouter.test.js
+++ back-end/tests/registerRouter.test.js
@@ -5,9 +5,84 @@ const expect = chai.expect
 
 chai.use(chaiHttp) 
 
-describe("Backend API - GET /sample", () => {
-    describe("Successful GET Request", () => {
-        //re add code after..
+describe("Backend API - POST /register", () => {
+    describe("Successful Registration", () => {
+        it("it should successfully register a new user")
+        const new_user = {
+            email: "test@test",
+            password: "test",
+        };
+
+        chai 
+            .request(app)
+            .get("/register")
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(201);
+                expect(res.body).to.have.property("message", "You have succefully logged in!");
+                done();
+            });
     });
+
+    describe("Failed Registration Because Missing Email", () => {
+        it("it should give an error if the email is missing", (done) => {
+            const invalid_user = {
+                password: "test",
+            };
+    
+            chai 
+                .request(app)
+                .post("/register")
+                .send(invalid_user)
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(500);
+                    expect(res.body).to.have.property("message", "ERROR: Cannot register user. Missing email.");
+                    done();
+                });
+        });
+    });
+
+
+    describe("Failed Registration Because Missing Password", () => {
+        it("it should give an error if the email is missing", (done) => {
+            const invalid_user = {
+                email: "test@test",
+            };
+    
+            chai 
+                .request(app)
+                .post("/register")
+                .send(invalid_user)
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(500);
+                    expect(res.body).to.have.property("message", "ERROR: Cannot register user. Missing password.");
+                    done();
+                });
+        });
+    });
+
+
+    describe("Failed Registration Because User Already Exists", () => {
+        it("it should give an error if the user tries to register again under the same email. Please login instead", (done) => {
+            const existing_user = {
+                email: "test@test",
+            };
+    
+            chai 
+                .request(app)
+                .post("/register")
+                .send(existing_user)
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(500);
+                    expect(res.body).to.have.property("message", "ERROR: Cannot register user. A user already exists under that email");
+                    done();
+                });
+        });
+    });
+    
+
 
 });

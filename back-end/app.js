3016788da919695199f14491dbb01461cfa80ffa
Kyoung655 <83367886+Kyoung655@users.noreply.github.com>
2024-04-16 03:10:07 -0400

    Revert "User story/7/99/posts with database"

--- back-end/app.js
+++ back-end/app.js
@@ -3,13 +3,8 @@ require('dotenv').config(); //load the .env file
 const express = require("express") // framework for building web applications
 const cors = require("cors") // middleware for enabling Cross-Origin Resource Sharing (CORS)
 const morgan = require("morgan") // middleware for logging HTTP requests in a readable format
-const path = require('path'); // Add this line to import the path module
-
 
 const app = express() // instantiate an Express object
-require('./models/user')
-require('./models/post')
-
 const mongoose = require('mongoose'); //use mongoose
 
 // import routes
@@ -20,13 +15,10 @@ const postRouter = require("./routes/postRouter")
 const discoverRouter = require("./routes/discoverRouter")
 const profileRouter = require("./routes/profileRouter")
 
-
 // use middleware
 app.use(morgan("dev")) // use morgan with dev style for logging HTTP requests
 app.use(cors()) // allows cross-origin resource sharing
 app.use(express.json())
-app.use('/Images', express.static(path.join(__dirname, 'Images'))); // configure to serve static files
-
 
 // use routers
 app.use("/feed", feedRouter)

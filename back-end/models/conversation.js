648c33690bf38ed05ab4d5aa45322f69a64a20ee
afknero <en1135@nyu.edu>
2024-04-18 12:38:23 -0400

    Add conversation and message models

---
+++ back-end/models/conversation.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const { ObjectId } = Schema.Types
+
+const conversationSchema = new Schema(
+  {
+    participants: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["active", "archived"],
+      default: "active",
+    },
+    messages: [
+      {
+        type: ObjectId,
+        ref: "Message",
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+const ConversationModel = mongoose.model("Conversation", conversationSchema)
+
+module.exports = ConversationModel

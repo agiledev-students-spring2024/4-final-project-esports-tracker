c0f5375359246a55165094f6cc9069f509471420
afknero <en1135@nyu.edu>
2024-04-27 21:42:56 -0400

    Implement room-based messaging for specific chat conversations

--- back-end/routes/messageRouter.js
+++ back-end/routes/messageRouter.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('user')
 const Conversation = require('../models/conversation')
+const Message = require('../models/message')
 
 router.get('/conversations/:username', async (req, res) => {
   const { username } = req.params
@@ -12,20 +13,24 @@ router.get('/conversations/:username', async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found.' })
     }
+
     // find all conversations where the user is a participant
     const conversations = await Conversation.find({ participants: user._id })
       .select('_id participants messages')
       .lean()
+
     // extract participant object IDs from conversations
     const participantIds = conversations.map((conversation) =>
       conversation.participants.find(
         (id) => id.toString() !== user._id.toString()
       )
     )
+
     // find participant details
     const participants = await User.find({ _id: { $in: participantIds } })
       .select('_id username pfp')
       .lean()
+
     // map recipient information to conversations
     const results = conversations.map((conversation) => {
       const recipientId = conversation.participants.find(
@@ -40,10 +45,60 @@ router.get('/conversations/:username', async (req, res) => {
         messages: conversation.messages,
       }
     })
+
     res.json({ results })
   } catch (error) {
     console.error('Error fetching conversations:', error)
     res.status(500).send('An error occurred while fetching conversations.')
+  }
+})
+
+router.get('/chat/:chatId/:username', async (req, res) => {
+  const { chatId, username } = req.params
+  try {
+    // find the user based on their username
+    const user = await User.findOne({ username }).select('_id')
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' })
+    }
+
+    // fetch and return all messages in the conversation
+    const messages = await Message.find({ conversation: chatId }).sort({ createdAt: -1 })
+
+    // find the other user
+    const otherUserId =
+      messages[0].sender.toString() === user._id.toString()
+        ? messages[0].receiver
+        : messages[0].sender
+
+    const otherUser = await User.findById(otherUserId).select('_id username pfp')
+
+    res.json({ sender: user, receiver: otherUser, messages: messages })
+  } catch (error) {
+    console.error('Error fetching chat:', error)
+    res.status(500).send('An error occurred while fetching chat.')
+  }
+})
+
+router.post('/chat/create', async (req, res) => {
+  try {
+    const { conversationId, senderId, receiverId, messageContent } = req.body
+
+    // create a new message
+    const newMessage = new Message({
+      conversation: conversationId,
+      sender: senderId,
+      receiver: receiverId,
+      message: messageContent,
+    })
+
+    // save the new message to the database
+    await newMessage.save()
+
+    res.status(201).json({ message: 'Message created successfully' })
+  } catch (error) {
+    console.error('Error creating message:', error)
+    res.status(500).json({ error: 'An error occurred while creating message.' })
   }
 })
 
--- front-end/src/pages/Message/Chat.js
+++ front-end/src/pages/Message/Chat.js
@@ -1,23 +1,50 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { IoChevronBack, IoPersonCircleOutline, IoSend } from "react-icons/io5"
-import "./Chat.css"
-import io from "socket.io-client"
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { IoChevronBack, IoSend } from 'react-icons/io5'
+import './Chat.css'
+import io from 'socket.io-client'
 
 const Chat = () => {
   const [messages, setMessages] = useState([])
-  const [messageInput, setMessageInput] = useState("")
+  const [messageInput, setMessageInput] = useState('')
   const [socket, setSocket] = useState(null)
+  const [sender, setSender] = useState(null)
+  const [receiver, setReceiver] = useState(null)
+  const { chatId } = useParams()
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3001")
+    const fetchChat = async () => {
+      try {
+        const userData = JSON.parse(localStorage.getItem('user'))['data']
+        if (userData) {
+          const { data } = await axios.get(
+            `http://localhost:3001/message/chat/${chatId}/${userData.user}`
+          )
+          setMessages(data.messages)
+          setSender(data.sender)
+          setReceiver(data.receiver)
+        }
+      } catch (error) {
+        console.error('Error fetching conversations:', error)
+      }
+    }
+    fetchChat()
+
+    // set up socket connection
+    const newSocket = io('http://localhost:3001')
+    setSocket(newSocket)
 
     // add an event listener for incoming messages
-    newSocket.on("chat message", (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg])
+    newSocket.on('chat message', (data) => {
+      console.log(data)
+      if (data.chatId == chatId) {
+        setMessages((prevMessages) => [ data.message, ...prevMessages])
+      }
     })
 
-    setSocket(newSocket)
+    // join the room associated with the current chat
+    newSocket.emit('join room', chatId)
 
     // cleanup function for when the component unmounts
     return () => {
@@ -25,13 +52,38 @@ const Chat = () => {
     }
   }, [setSocket])
 
-  const sendMessage = (e) => {
+  const sendMessage = async (e) => {
     e.preventDefault()
 
-    if (!socket || !messageInput.trim()) return
+    if (!messageInput.trim()) return
 
-    socket.emit("chat message", messageInput)
-    setMessageInput("")
+    try {
+      if (socket) {
+        // emit message to other user
+        socket.emit('chat message', {
+          chatId: chatId,
+          sender: sender._id,
+          message: messageInput,
+        })
+
+        // save the message to the database
+        await axios.post('http://localhost:3001/message/chat/create', {
+          conversationId: chatId,
+          senderId: sender._id,
+          receiverId: receiver._id,
+          messageContent: messageInput,
+        })
+
+        // clear the message input
+        setMessageInput('')
+      }
+    } catch (error) {
+      console.error('Error sending message:', error)
+    }
+  }
+
+  if (!receiver) {
+    return <div>Loading...</div>
   }
 
   return (
@@ -40,18 +92,22 @@ const Chat = () => {
         <Link to="/message">
           <IoChevronBack size={32} />
         </Link>
-        <div className="petOwnersName">Pet Owner's Name</div>
+        <div className="petOwnersName">{receiver.username}</div>
         <Link to="/profile">
-          <IoPersonCircleOutline size={40} />
+          <img src={receiver.pfp} alt="avatar" />
         </Link>
       </div>
-
       <div className="chatContent">
         <div className="chatMessages">
           {messages.map((msg, i) => (
-            <div className="chatSent" key={i}>
+            <div
+              className={
+                msg.sender === sender._id ? 'chatSent' : 'chatReceived'
+              }
+              key={i}
+            >
               <div className="chatBubble">
-                <div className="chatMessage">{msg}</div>
+                <div className="chatMessage">{msg.message}</div>
               </div>
             </div>
           ))}
